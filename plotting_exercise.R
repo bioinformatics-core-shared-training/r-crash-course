c01bf31503a6ca7f78b73562e33ac380e6e7b73c
Mark Dunning <mark.dunning@gmail.com>
2018-03-25 18:09:31 +0100

    more information on plotting layouts
    show example of what to aim for in plotting exercise

---
+++ plotting_exercise.R
@@ -0,0 +1,11 @@
+gapminder <- read.csv("gapminder.csv")
+png("plotting_exercise_target.png")
+plot(gapminder$pop,gapminder$lifeExp,pch=16,
+     ylab="Life Expectancy",
+     xlab="Population",main="Life Expectancy trend with population")
+
+
+outliers <- gapminder$pop > 3.5e8
+points(gapminder$pop[outliers],gapminder$lifeExp[outliers],col="red",pch=16)
+abline(v=3.5e8,lty=2,col="red")
+dev.off()
